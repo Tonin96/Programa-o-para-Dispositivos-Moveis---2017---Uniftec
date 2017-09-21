3fe6380d43a71cdbde8383dbfb3a510a7ab89b75
bmarsilio <bruno.marsilio@hotmail.com>
2017-09-21 20:23:11 -0300

    agora sim FtecListView

--- FtecListView/app/src/main/java/br/com/uniftec/fteclistview/MainActivity.java
+++ FtecListView/app/src/main/java/br/com/uniftec/fteclistview/MainActivity.java
@@ -1,13 +1,18 @@
 package br.com.uniftec.fteclistview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewFilmes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listViewFilmes = (ListView)findViewById(R.id.list_view_filmes);
     }
 }
