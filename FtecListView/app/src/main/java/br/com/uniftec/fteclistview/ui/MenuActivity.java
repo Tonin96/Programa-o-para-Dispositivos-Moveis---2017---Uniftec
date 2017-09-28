22a399634bd9bf5a2c587bf2d239bc2701150513
bmarsilio <bruno.marsilio@hotmail.com>
2017-09-28 20:45:20 -0300

    iniciada implementacao do menu no projeto FtevListView

---
+++ FtecListView/app/src/main/java/br/com/uniftec/fteclistview/ui/MenuActivity.java
@@ -0,0 +1,16 @@
+package br.com.uniftec.fteclistview.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.uniftec.fteclistview.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+}
