effb202b6373d60e1bb2ff2407049fecfa7ed88e
bmarsilio <bruno.marsilio@hotmail.com>
2017-11-17 01:49:53 -0200

    criados metodos da pedidoService

--- EcommerceMobile/app/src/main/java/br/com/uniftec/ecommercemobile/services/PedidoService.java
+++ EcommerceMobile/app/src/main/java/br/com/uniftec/ecommercemobile/services/PedidoService.java
@@ -1,20 +1,27 @@
 package br.com.uniftec.ecommercemobile.services;
 
+import java.util.List;
+
 import br.com.uniftec.ecommercemobile.model.EcommerceResponse;
 import br.com.uniftec.ecommercemobile.model.Pedido;
-import br.com.uniftec.ecommercemobile.model.UsuarioResponse;
+import br.com.uniftec.ecommercemobile.model.PedidoResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface PedidoService {
 
+    @GET("/pedido")
+    public Call<EcommerceResponse<List<PedidoResponse>>> carregarPedidos(@Header("X-Token") String token);
+
     @PUT("/pedido")
-    public Call<EcommerceResponse<String>> salvarPedido(@Body Pedido pedido);
+    public Call<EcommerceResponse<PedidoResponse>> salvarPedido(@Body Pedido pedido, @Header("X-Token") String token);
 
     @POST("/pedido")
-    public Call<EcommerceResponse<UsuarioResponse>> atualizarPedido(@Body Pedido pedido, @Header("X-Token") String token);
+    public Call<EcommerceResponse<PedidoResponse>> atualizarPedido(@Path("id") Long id);
 
 }
