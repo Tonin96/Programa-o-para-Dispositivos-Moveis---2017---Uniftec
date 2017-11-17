505e6972ada8443fdf6a42a766242d10227b1de4
bmarsilio <bruno.marsilio@hotmail.com>
2017-11-17 01:57:39 -0200

    validada usuarioService

--- EcommerceMobile/app/src/main/java/br/com/uniftec/ecommercemobile/services/UsuarioService.java
+++ EcommerceMobile/app/src/main/java/br/com/uniftec/ecommercemobile/services/UsuarioService.java
@@ -1,6 +1,7 @@
 package br.com.uniftec.ecommercemobile.services;
 
 import br.com.uniftec.ecommercemobile.model.EcommerceResponse;
+import br.com.uniftec.ecommercemobile.model.Login;
 import br.com.uniftec.ecommercemobile.model.Usuario;
 import br.com.uniftec.ecommercemobile.model.UsuarioEndereco;
 import br.com.uniftec.ecommercemobile.model.UsuarioResponse;
@@ -12,26 +13,23 @@ import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
-/**
- * TODO: ver os parâmetros do método @POST("/usuario/login")
- * */
 public interface UsuarioService {
 
     @GET("/usuario")
     public Call<EcommerceResponse<UsuarioResponse>> carregarUsuario(@Header("X-Token") String token);
 
+    @POST("/usuario")
+    public Call<EcommerceResponse<UsuarioResponse>> atualizarUsuario(@Header("X-Token") String token, @Body Usuario usuario);
+
     @PUT("/usuario")
     public Call<EcommerceResponse<String>> salvarUsuario(@Body Usuario usuario);
 
-    @POST("/usuario")
-    public Call<EcommerceResponse<UsuarioResponse>> atualizarUsuario(@Body Usuario usuario, @Header("X-Token") String token);
+    @PUT("/usuario/endereco")
+    public Call<EcommerceResponse<UsuarioResponse>> salvarUsuarioEndereco(@Header("X-Token") String token, @Body UsuarioEndereco usuarioEndereco);
 
     @PUT("/usuario/endereco")
-    public Call<EcommerceResponse<UsuarioResponse>> salvarUsuarioEndereco(@Body UsuarioEndereco usuarioEndereco, @Header("X-Token") String token);
-
-    @PUT("/usuario/endereco")
-    public Call<EcommerceResponse<UsuarioResponse>> removerUsuarioEndereco(@Path("id") String id, @Header("X-Token") String token);
+    public Call<EcommerceResponse<UsuarioResponse>> removerUsuarioEndereco(@Header("X-Token") String token, @Path("id") String id);
 
     @POST("/usuario/login")
-    public Call<EcommerceResponse<String>> loginUsuario(@Body Usuario usuario);
+    public Call<EcommerceResponse<String>> loginUsuario(@Body Login login);
 }
