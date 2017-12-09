5a2d64b0266e407323d6484cd0474a91c23e1c87
bmarsilio <bruno.marsilio@hotmail.com>
2017-12-09 14:44:24 -0200

    implementado para mostrar o endereco de entrega no pedido

--- EcommerceMobile/app/src/main/java/br/com/uniftec/ecommercemobile/ui/PedidoActivity.java
+++ EcommerceMobile/app/src/main/java/br/com/uniftec/ecommercemobile/ui/PedidoActivity.java
@@ -7,16 +7,27 @@ import android.widget.TextView;
 
 import br.com.uniftec.ecommercemobile.R;
 import br.com.uniftec.ecommercemobile.model.Pedido;
+import br.com.uniftec.ecommercemobile.model.UsuarioEndereco;
+import br.com.uniftec.ecommercemobile.model.UsuarioEnderecoResponse;
 
 public class PedidoActivity extends AbstractActivity {
 
     public static final String PEDIDO_PARAMETER = "PEDIDO_PARAMETER";
 
     private Pedido pedido;
+    private UsuarioEnderecoResponse enderecoEntrega;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-    private TextView tituloPedido;
+    private TextView titulo;
+    private TextView data;
+    private TextView status;
+    private TextView bairro;
+    private TextView cidade;
+    private TextView complemento;
+    private TextView estado;
+    private TextView logradouro;
+    private TextView numero;
 
     public PedidoActivity() {
 
@@ -29,11 +40,28 @@ public class PedidoActivity extends AbstractActivity {
 
     @Override
     protected void setupView() {
-        tituloPedido = (TextView) findViewById(R.id.activity_pedido_titulo);
+        titulo = (TextView) findViewById(R.id.activity_pedido_titulo);
+        data = (TextView) findViewById(R.id.activity_pedido_data);
+        status = (TextView) findViewById(R.id.activity_pedido_status);
+        bairro = (TextView) findViewById(R.id.activity_pedido_bairro);
+        cidade = (TextView) findViewById(R.id.activity_pedido_cidade);
+        complemento = (TextView) findViewById(R.id.activity_pedido_complemento);
+        estado = (TextView) findViewById(R.id.activity_pedido_estado);
+        logradouro = (TextView) findViewById(R.id.activity_pedido_logradouro);
+        numero = (TextView) findViewById(R.id.activity_pedido_numero);
 
         pedido = (Pedido) getIntent().getSerializableExtra(PEDIDO_PARAMETER);
+        enderecoEntrega = pedido.getEnderecoEntrega();
 
-        tituloPedido.setText("Pedido " + pedido.getStatus());
+        titulo.setText(pedido.getId().toString());
+        data.setText(pedido.getData());
+        status.setText(pedido.getStatus());
+        bairro.setText(enderecoEntrega.getBairro());
+        cidade.setText(enderecoEntrega.getCidade());
+        complemento.setText(enderecoEntrega.getComplemento());
+        estado.setText(enderecoEntrega.getEstado());
+        logradouro.setText(enderecoEntrega.getLogradouro());
+        numero.setText(enderecoEntrega.getNumero());
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view_pedido);
         recyclerView.setHasFixedSize(true);
