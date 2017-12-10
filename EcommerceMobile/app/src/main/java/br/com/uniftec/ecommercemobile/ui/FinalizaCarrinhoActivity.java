0585ca8e42d67b6e016764123551aace26b387e3
Bruno Tonin <tonin1234@hotmail.com>
2017-12-10 13:48:44 -0200

    feito limpar o carrinho quando finaliza

--- EcommerceMobile/app/src/main/java/br/com/uniftec/ecommercemobile/ui/FinalizaCarrinhoActivity.java
+++ EcommerceMobile/app/src/main/java/br/com/uniftec/ecommercemobile/ui/FinalizaCarrinhoActivity.java
@@ -1,18 +1,25 @@
 package br.com.uniftec.ecommercemobile.ui;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
+import br.com.uniftec.ecommercemobile.MainActivity;
 import br.com.uniftec.ecommercemobile.R;
 import br.com.uniftec.ecommercemobile.adapter.ListaEnderecoAdapter;
 import br.com.uniftec.ecommercemobile.model.Pedido;
@@ -25,12 +32,13 @@ import br.com.uniftec.ecommercemobile.model.UsuarioResponse;
 import br.com.uniftec.ecommercemobile.services.CarrinhoService;
 import br.com.uniftec.ecommercemobile.task.SalvarPedidoTask;
 
-public class FinalizaCarrinhoActivity extends AbstractActivity  implements SalvarPedidoTask.SalvarPedidoDelegate, View.OnClickListener{
+public class FinalizaCarrinhoActivity extends AbstractActivity implements SalvarPedidoTask.SalvarPedidoDelegate, View.OnClickListener {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private SharedPreferences preferences;
+    private ProgressDialog progressDialog;
     private EditText numeroCartao;
     private EditText vencimentoCartao;
     private EditText cvvCartao;
@@ -80,55 +88,106 @@ public class FinalizaCarrinhoActivity extends AbstractActivity  implements Salva
 
     @Override
     public void onClick(View view) {
-        if(view.getId() == R.id.activity_finaliza_carrinho_button_finalizar){
-            Pedido pedido = new Pedido();
+        if (view.getId() == R.id.activity_finaliza_carrinho_button_finalizar) {
 
+            if(validarCamposObrigatorios()) {
+                Pedido pedido = new Pedido();
 
-            ArrayList<PedidoProdutoResponse> pedidoProdutos = new ArrayList<PedidoProdutoResponse>();
-            ArrayList<Produto> produtosCarrinho = carrinhoService.buscaProdutos();
+                ArrayList<PedidoProdutoResponse> pedidoProdutos = new ArrayList<PedidoProdutoResponse>();
+                ArrayList<Produto> produtosCarrinho = carrinhoService.buscaProdutos();
 
-            ArrayList<Long> arrayProdutosId = new ArrayList<>();
-            for (Produto produto : produtosCarrinho){
-                Log.d("produtoID", String.valueOf(produto.getId()));
-                arrayProdutosId.add(produto.getId());
+                ArrayList<Long> arrayProdutosId = new ArrayList<>();
+                for (Produto produto : produtosCarrinho) {
+                    arrayProdutosId.add(produto.getId());
+                }
+
+                pedido.setProdutos(arrayProdutosId);
+                pedido.setCartaoCredito(String.valueOf(numeroCartao.getText()));
+                pedido.setCodigoSeguranca(String.valueOf(cvvCartao.getText()));
+                pedido.setValidadeCartaoCredito(String.valueOf(vencimentoCartao.getText()));
+
+                Gson gson = new Gson();
+                String usuario = preferences.getString("usuario", "null");
+                if (!usuario.equals("null")) {
+                    retornoJsonUsuarioResponse = gson.fromJson(usuario, UsuarioResponse.class);
+                }
+
+                Log.d("Enviado", gson.toJson(pedido));
+
+                for (UsuarioEnderecoResponse usuarioEnderecoResponse : retornoJsonUsuarioResponse.getEnderecos()) {
+                    pedido.setIdEnderecoUsuario(usuarioEnderecoResponse.getId());
+                }
+                SalvarPedidoTask salvarPedidoTask = new SalvarPedidoTask(this);
+
+                Object[] parametros = new Object[2];
+                parametros[0] = token;
+                parametros[1] = pedido;
+
+                progressDialog(this, "Enviando");
+                salvarPedidoTask.execute(parametros);
             }
-
-            Integer[] teste  = new Integer[1];
-            teste[0] = 230;
-            pedido.setProdutos(arrayProdutosId);
-            pedido.setCartaoCredito(String.valueOf(numeroCartao.getText()));
-            pedido.setCodigoSeguranca(String.valueOf(cvvCartao.getText()));
-            pedido.setValidadeCartaoCredito(String.valueOf(vencimentoCartao.getText()));
-
-            Gson gson = new Gson();
-            String usuario = preferences.getString("usuario", "null");
-            if (!usuario.equals("null")) {
-                retornoJsonUsuarioResponse = gson.fromJson(usuario, UsuarioResponse.class);
-            }
-
-            Log.d("teste", gson.toJson(pedido));
-
-            for (UsuarioEnderecoResponse usuarioEnderecoResponse : retornoJsonUsuarioResponse.getEnderecos()) {
-                pedido.setIdEnderecoUsuario(usuarioEnderecoResponse.getId());
-            }
-            Log.d("token", token);
-            SalvarPedidoTask salvarPedidoTask = new SalvarPedidoTask(this);
-
-            Object[] parametros = new Object[2];
-            parametros[0] = token;
-            parametros[1] = pedido;
-
-            salvarPedidoTask.execute(parametros);
         }
     }
 
     @Override
     public void salvarPedidoSucesso(PedidoResponse pedidoResponse) {
-        Log.d("Sucesso", "sucesso");
+
+        SharedPreferences sharedPreferences = getSharedPreferences("carrinho", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit().clear();
+        editor.commit();
+        dismisProgressDialog();
+        Toast.makeText(this, "Pedido Enviado", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void salvarPedidoFalha(String mensagem) {
+        dismisProgressDialog();
+
         Log.d("Falha", mensagem);
     }
+
+    public void progressDialog(Activity activity, String mensagem) {
+        progressDialog = ProgressDialog.show(activity, "Aguarde", mensagem, true, false);
+    }
+
+    public void dismisProgressDialog() {
+        progressDialog.dismiss();
+        progressDialog = null;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+
+
+            finish();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean validarCamposObrigatorios() {
+        boolean validacao = true;
+
+        if(Objects.equals(String.valueOf(numeroCartao.getText()), "")){
+           numeroCartao.setError("Preencha o numero do cartao");
+            validacao = false;
+        }
+
+        if(Objects.equals(String.valueOf(cvvCartao.getText()), "")){
+            cvvCartao.setError("Preencha o CVV");
+            validacao = false;
+        }
+        if(Objects.equals(String.valueOf(vencimentoCartao.getText()), "")){
+            vencimentoCartao.setError("Preencha a data de vencimento do cartao");
+            validacao = false;
+        }
+
+        return validacao;
+    }
+
 }
